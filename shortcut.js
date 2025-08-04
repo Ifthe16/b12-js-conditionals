d612494db44d371cd5d897e6fa30f624723f1bb3
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-04 16:50:44 +0600

    added shortcut.js

---
+++ shortcut.js
@@ -0,0 +1,26 @@
+const isLeader = true;
+// if(isLeader === true)
+if (isLeader) {
+    console.log('leader asche! chair charo');
+}
+else {
+    console.log('vitore dukte dibi na');
+}
+
+const isPassed = false;
+// if (isPassed === false)
+if (!isPassed) {
+    console.log('toke biya diya dimu');
+}
+else {
+    console.log('tumi amader noyon er moni');
+}
+
+const isVerified = true;
+// if(isVerified === false)
+if (!!isVerified) {
+    console.log('Welcome')
+}
+else {
+    console.log('Invalid')
+}
